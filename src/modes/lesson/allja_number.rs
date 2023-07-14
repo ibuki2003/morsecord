03ac62c9e6ff56f7dc41936e9147f84aff973eff
taka2 <taka23131@gmail.com>
2023-07-14 15:12:54 +0900

    refactor: let AllJANumberGen have allja_nr

--- src/modes/lesson/allja_number.rs
+++ src/modes/lesson/allja_number.rs
@@ -1,9 +1,10 @@
 use rand::{self, Rng};
-pub struct AllJANumberGen;
-impl Iterator for AllJANumberGen {
-    type Item = String;
+pub struct AllJANumberGen {
+    allja_nr: Vec<String>,
+}
 
-    fn next(&mut self) -> Option<Self::Item> {
+impl AllJANumberGen {
+    pub fn new() -> Self {
         let allja_nr = [
             (101..=114).map(|x| x.to_string()).collect::<Vec<_>>(),
             (2..=48)
@@ -17,8 +18,16 @@ impl Iterator for AllJANumberGen {
                 .collect::<Vec<_>>(),
         ]
         .concat();
+        Self { allja_nr }
+    }
+}
+
+impl Iterator for AllJANumberGen {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
         let mut rng = rand::thread_rng();
-        let s = "5NN ".to_owned() + &allja_nr[rng.gen_range(0..allja_nr.len())];
+        let s = "5NN ".to_owned() + &self.allja_nr[rng.gen_range(0..self.allja_nr.len())];
         Some(match rand::random::<u8>() {
             0..=99 => s + "H",
             100..=199 => s + "M",
@@ -29,7 +38,7 @@ impl Iterator for AllJANumberGen {
 
 #[test]
 fn test_allja_number() {
-    let mut gen = AllJANumberGen;
+    let mut gen = AllJANumberGen::new();
     for _ in 0..100 {
         println!("{}", gen.next().unwrap());
     }
