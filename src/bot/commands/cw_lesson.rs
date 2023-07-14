e7dd8b641b9d71464f73fd4a19bea11a230d2071
ibuki2003 <main@fuwa.dev>
2023-07-14 15:21:02 +0900

    feat(lesson): select problem set by argument

--- src/bot/commands/cw_lesson.rs
+++ src/bot/commands/cw_lesson.rs
@@ -5,6 +5,23 @@ use std::sync::{Arc, Mutex};
 
 use crate::bot::BotStateMode;
 
+pub fn get_lesson_gen(probset: &str) -> Result<Box<dyn Iterator<Item = String> + Send>, String> {
+    let (probset_name, _probset_args_str) =
+        probset.split_once(':').unwrap_or((probset, ""));
+
+    use crate::modes::lesson;
+
+    let gen: Box<dyn Iterator<Item = String> + Send> = match probset_name {
+        "call_ja" => Box::new(lesson::callsign::JaCallsignGen {}),
+        "nr_allja" => Box::new(lesson::allja_number::AllJANumberGen::new()),
+        _ => return Err(
+            "unknown probset.\n".to_owned()
+                + "available selections are: call_ja, nr_allja",
+            ),
+    };
+    Ok(gen)
+}
+
 impl crate::bot::Bot {
     pub async fn run_command_lesson_start(
         &self,
@@ -15,23 +32,27 @@ impl crate::bot::Bot {
         let mut max_speed = None;
         let mut min_freq = None;
         let mut max_freq = None;
+        let mut probset = "call_ja".to_string();
 
         command
             .data
             .options
             .iter()
             .try_fold::<_, _, Result<(), String>>((), |_, x| {
-                let v = x
-                    .value
-                    .as_ref()
-                    .map_or(Err("value not found".to_string()), |v| {
-                        v.as_f64().ok_or("value is not f64".to_string())
-                    })? as f32;
+                let v = x.value.as_ref().ok_or("value not found".to_string())?;
+
+                let vf = v
+                    .as_f64()
+                    .map(|x| x as f32)
+                    .ok_or("value is not f64".to_string());
+                let vs = v.as_str().ok_or("value is not string".to_string());
+
                 match x.name.as_str() {
-                    "min_speed" => min_speed = Some(v),
-                    "max_speed" => max_speed = Some(v),
-                    "min_freq" => min_freq = Some(v),
-                    "max_freq" => max_freq = Some(v),
+                    "min_speed" => min_speed = Some(vf?),
+                    "max_speed" => max_speed = Some(vf?),
+                    "min_freq" => min_freq = Some(vf?),
+                    "max_freq" => max_freq = Some(vf?),
+                    "probset" => probset = vs?.to_string(),
                     _ => (),
                 };
                 Ok(())
@@ -53,11 +74,15 @@ impl crate::bot::Bot {
 
         let speed_range = min_speed..=max_speed;
         let freq_range = min_freq..=max_freq;
+
+        probset.make_ascii_lowercase();
+        let gen = get_lesson_gen(&probset)?;
+
         let gid = command.guild_id.ok_or("not in guild")?;
         let state = Arc::new(Mutex::new(crate::modes::lesson::LessonModeState::new(
             speed_range,
             freq_range,
-            Box::new(crate::modes::lesson::callsign::JaCallsignGen {}),
+            gen,
         )));
         crate::modes::lesson::start(&ctx, gid, state.clone())
             .await
@@ -112,6 +137,13 @@ impl crate::bot::Bot {
                         .description("maximum freq")
                         .kind(serenity::model::prelude::command::CommandOptionType::Number)
                         .min_number_value(200.0)
+                        .required(false)
+                })
+                .create_option(|option| {
+                    option
+                        .name("probset")
+                        .description("problem set name (can be followed by colon and args)")
+                        .kind(serenity::model::prelude::command::CommandOptionType::String)
                         .required(false)
                 })
         })
